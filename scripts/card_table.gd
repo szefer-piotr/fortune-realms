0aaf2b16da3e3397610c679dbc64b87ac73fc611
Piotr Szefer <szefer85@gmail.com>
2025-08-31 17:42:45 +0200

    Refactor card draw logic

--- scripts/card_table.gd
+++ scripts/card_table.gd
@@ -18,22 +18,14 @@ func _ready():
 		$StarMenu.add_item(str(i), i)
 
 func _on_draw_pressed():
-	var values = [1,2,3,4,5,6,7,8,9,10,"A","Star"]
-	var choice = values[randi() % values.size()]
-	var card = card_scene.instantiate()
-	$CardsContainer.add_child(card)
-	card.position = Vector2(cards.size() * 60, 0)
-	cards.append(card)
-	card.set_value(choice)
-	if choice == "A":
-		current_card = card
-		$AceMenu.popup()
-	elif choice == "Star":
-		current_card = card
-		$StarMenu.popup()
-	else:
-		total_score += int(choice)
-		_update_score()
+        var num := randi() % 6 + 1
+        var card = card_scene.instantiate()
+        $CardsContainer.add_child(card)
+        card.position = Vector2(cards.size() * 60, 0)
+        cards.append(card)
+        card.set_number(num)
+        total_score += num
+        _update_score()
 
 func _on_ace_selected(id):
 	current_card.set_value(id)
