1b444365777bc631c75b1a5b00cc989822681cc2
Piotr Szefer <szefer85@gmail.com>
2025-09-04 08:12:10 +0200

    WIP experimenting wihth the layout.

--- scripts/Card.gd
+++ scripts/Card.gd
@@ -6,20 +6,20 @@ extends Control
 var _face_up: bool = false
 
 func show_front() -> void:
-    front.visible = true
-    back.visible = false
-    _face_up = true
+	front.visible = true
+	back.visible = false
+	_face_up = true
 
 func show_back() -> void:
-    front.visible = false
-    back.visible = true
-    _face_up = false
+	front.visible = false
+	back.visible = true
+	_face_up = false
 
 func flip() -> void:
-    if _face_up:
-        show_back()
-    else:
-        show_front()
+	if _face_up:
+		show_back()
+	else:
+		show_front()
 
 func is_face_up() -> bool:
-    return _face_up
+	return _face_up
