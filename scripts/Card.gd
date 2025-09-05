ea2518189c00ca7c8211d2c0f2388a3b61edeb5c
Piotr Szefer <szefer85@gmail.com>
2025-09-05 07:18:44 +0200

    WIP: local changes

--- scripts/Card.gd
+++ scripts/Card.gd
@@ -3,7 +3,15 @@ extends Control
 @onready var front: TextureRect = $Front
 @onready var back: TextureRect = $Back
 
+@export var flip_time := 0.2
 var _face_up: bool = false
+
+func _ready() -> void:
+	_center_pivot()
+	resized.connect(_center_pivot)
+
+func _center_pivot() -> void:
+	pivot_offset = size / 2
 
 func show_front() -> void:
 	front.visible = true
@@ -15,7 +23,17 @@ func show_back() -> void:
 	back.visible = true
 	_face_up = false
 
+<<<<<<< HEAD
 func flip() -> void:
+=======
+func flip(duration: float = flip_time) -> void:
+	var tw := create_tween()
+	tw.tween_property(self, "scale:x", 0.0, duration / 2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+	tw.tween_callback(Callable(self, "_swap_face"))
+	tw.tween_property(self, "scale:x", 1.0, duration / 2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+
+func _swap_face() -> void:
+>>>>>>> 1d72b4ee72ed8cac31e964ac2a976377552ef88a
 	if _face_up:
 		show_back()
 	else:
