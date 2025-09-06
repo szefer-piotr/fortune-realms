dd41005bfea66ae00052d88d42808de72806a7cf
Piotr Szefer <szefer85@gmail.com>
2025-09-06 18:42:20 +0200

    WIP cards overlapp.

--- scripts/Card3D.gd
+++ scripts/Card3D.gd
@@ -1,20 +1,22 @@
 extends RigidBody3D
 
-const FACE_TEXTURES := [
-    preload("res://assets/cards_png/gpt_game_assets/cards_simple/coins.png"),
-    preload("res://assets/cards_png/gpt_game_assets/cards_simple/thief.png"),
+const FACE_TEXTURES: Array[Texture2D] = [
+	preload("res://assets/cards_png/gpt_game_assets/cards_simple/coins.png"),
+	preload("res://assets/cards_png/gpt_game_assets/cards_simple/thief.png"),
 ]
 
-var number_value: int
+var number_value: int = 0
 
 @onready var number_label: Label3D = $NumberLabel
 @onready var front: MeshInstance3D = $Front
 
 func _ready() -> void:
-    number_value = randi_range(1, 6)
-    number_label.text = str(number_value)
+	number_value = randi_range(1, 6)
+	number_label.text = str(number_value)
 
-    var texture := FACE_TEXTURES[randi() % FACE_TEXTURES.size()]
-    var mat := front.get_active_material(0).duplicate()
-    mat.albedo_texture = texture
-    front.material_override = mat
+	var texture: Texture2D = FACE_TEXTURES[randi() % FACE_TEXTURES.size()]
+	var src_mat := front.get_active_material(0)
+	if src_mat:
+		var mat: StandardMaterial3D = (src_mat.duplicate() as StandardMaterial3D)
+		mat.albedo_texture = texture
+		front.material_override = mat
