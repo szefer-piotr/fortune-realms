926e369140a9e681373724dfd38db5e0b2982782
Piotr Szefer <szefer85@gmail.com>
2025-09-07 08:35:56 +0200

    Bugged cards.

--- scripts/Card3D.gd
+++ scripts/Card3D.gd
@@ -5,5 +5,5 @@ var number_value: int = 0
 @onready var number_label: Label = $NumberViewport/NumberLabel
 
 func _ready() -> void:
-    number_value = randi_range(1, 6)
-    number_label.text = str(number_value)
+	number_value = randi_range(1, 6)
+	number_label.text = str(number_value)
