e4c52c50b9226f71ea0944e6cd17f1320da5fa92
Piotr Szefer <szefer85@gmail.com>
2025-09-09 07:50:50 +0200

    WIP Randomly change face.

--- scripts/Card3D.gd
+++ scripts/Card3D.gd
@@ -1,9 +1,9 @@
 extends RigidBody3D
 
 @export var face_textures: Array[Texture2D] = [
-        preload("res://assets/cards_png/gpt_game_assets/cards_simple/coins.png"),
-        preload("res://assets/cards_png/gpt_game_assets/cards_simple/draws.png"),
-        preload("res://assets/cards_png/gpt_game_assets/cards_simple/thief.png"),
+	preload("res://assets/cards_png/gpt_game_assets/cards_simple/coins.png"),
+	preload("res://assets/cards_png/gpt_game_assets/cards_simple/draws.png"),
+	preload("res://assets/cards_png/gpt_game_assets/cards_simple/thief.png"),
 ]
 
 var number_value: int = 0
@@ -11,8 +11,8 @@ var number_value: int = 0
 @onready var number_label: Label3D = $NumberLabel
 
 func _ready() -> void:
-        number_value = randi_range(1, 6)
-        number_label.text = str(number_value)
+	number_value = randi_range(1, 6)
+	number_label.text = str(number_value)
 
 func set_face_texture(tex: Texture2D) -> void:
-        $Front.material_override.albedo_texture = tex
+	$Front.material_override.albedo_texture = tex
